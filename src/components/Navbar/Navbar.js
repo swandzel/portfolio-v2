2d76f41a7963c9fe80807a668253f2bdb4bd40c2
swandzel <s.wandzel@o2.pl>
2022-08-22 13:42:54 +0200

    navbar animation on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,21 @@
-import React from "react";
 import "./Navbar.scss";
 import { navbar_links } from "../../variables/navbar-links";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [changeStyle, setChangeStyle] = useState(false);
+
+  useEffect(() => {
+    const changeStyleHandler = () => {
+      setChangeStyle(window.pageYOffset > 80);
+    };
+
+    window.addEventListener("scroll", changeStyleHandler);
+    return () => window.removeEventListener("scroll", changeStyleHandler);
+  }, []);
+
   return (
-    <nav>
+    <nav className={`navbar ${changeStyle ? "navbar--scroll" : ""}`}>
       <ul>
         {navbar_links.map((navbar_link) => (
           <li key={navbar_link.id}>{navbar_link.link}</li>
