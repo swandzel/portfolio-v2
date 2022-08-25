8272fd3b392c2054f679c6e9320f75e05112bdd5
swandzel <s.wandzel@o2.pl>
2022-08-25 17:49:53 +0200

    loader delete, header image height added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
 import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
@@ -6,35 +5,16 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
-import Loader from "./components/Loader/Loader";
 
 const App = () => {
-  const [showLoader, setShowLoader] = useState(true);
-
-  useEffect(() => {
-    const loaderToggle = () => {
-      setTimeout(() => {
-        setShowLoader(false);
-      }, 2000);
-    };
-    window.addEventListener("load", loaderToggle);
-    return () => window.addEventListener("load", loaderToggle);
-  }, []);
-
   return (
     <div className="App">
-      {showLoader ? (
-        <Loader />
-      ) : (
-        <>
-          <Navbar />
-          <Header />
-          <About />
-          <Projects />
-          <Contact />
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      <Header />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 };
