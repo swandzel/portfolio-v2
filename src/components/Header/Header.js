2318298c5c90f97dedfb7043903e62be5711744b
swandzel <s.wandzel@o2.pl>
2022-08-27 17:09:58 +0200

    new main photo, text style fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Button from "../Button/Button";
 import "./Header.scss";
-import MyPhoto from "../../assets/images/main-photo.png";
-import MyPhotoMobile from "../../assets/images/main-photo-mobile.png";
+import MyPhoto from "../../assets/images/main-photo-new.webp";
+import MyPhotoMobile from "../../assets/images/main-photo-mobile-new.webp";
 import { motion } from "framer-motion";
 import BracketsIcon from "../../assets/icons/icon-brackets-header.svg";
 import LaptopIcon from "../../assets/icons/icon-laptop-header.svg";
