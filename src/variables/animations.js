9f27357f980ca192697428f4d2a87b97e411b101
swandzel <s.wandzel@o2.pl>
2022-09-09 19:41:57 +0200

    performance fix

--- src/variables/animations.js
+++ src/variables/animations.js
@@ -5,5 +5,5 @@ export const fromBottom = {
 
 export const headerAnim = {
   initial: { opacity: 0 },
-  animate: { opacity: 1, transition: { delay: 0.5 } },
+  animate: { opacity: 1, transition: { delay: 1 } },
 };
